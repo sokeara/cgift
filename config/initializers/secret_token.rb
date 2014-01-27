d8d4a814798609e180437fb9f260c4476003afb0
sokeara <sim.sokeara@gmail.com>
2014-01-27 15:49:17 +0700

    please download project form github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cgift::Application.config.secret_key_base = '1398cc98941001879f93e862bf0791763c887b5a370fc907c5e590ae28c64103fd4198fd1474750602c7b757850c77d60b2b32ff0355a52ba8b36f6934624b19'
